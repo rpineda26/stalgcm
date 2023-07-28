ac21982c0cfe44bd3634617ffa30cf9fa1cb004d
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-07-28 18:54:33 +0800

    add: validator functions to make sure machine is deterministic

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ python3 main.py
 ```
 
 
-## :cat: text input format
+## :trident: text input format
 **Q**     *This is the list of states separated by space characters*
 **Sigma**    *This is the alphabet separated by space characters*
 **(Start, Final, Reject)**     *Tuple (a,b, c) representing the start, accepting, and reject states. These should exist in Q*
@@ -40,7 +40,7 @@ python3 main.py
 .
 **transition functions** 
 
-### restrictions
+## :x: restrictions
 1. let s be an element from sigma, s should never be = '-' or '+'
    - '-' is reserved for the left end marker
    - '+' is reserved for the right end marker
@@ -51,5 +51,11 @@ python3 main.py
 4. transition function from the accepting state should never move into another non accepting state
 5. transition function from the reject state should enver move into anbother non reject state
   
+## :dragon: test cases
+the tests folder contains two directories: one for the machine definitions, and the other is for sample inputs
+with expected outputs. The machine definition files will be named similarly along with their respective sample inputs
+for easy reference.
 
-
+1. sigma = {a,b} and w = even number 'a' and 'b' is divisible by 3
+2. sigma = {a,b} and w = starts with 'b' and ends with 'a' where 'a' always come in pairs
+3. sigma = {a,b} and w = (a+b)<sup>\*</sup>δ(a+b)<sup>\*</sup> and δ = baab
--- controller.py
+++ controller.py
@@ -2,18 +2,22 @@ from queue import Queue
 import sys
 
 """
+
+    Author : Ralph Dawson G. Pineda
+    Section : STALGCM S13
+    Model : 2-way Deterministic Finite Automata
     Todo:  verification of integrity of text input
     checklist:      do both start and final states exist in Q
                     does every element from every transition function exist in Q and sigma and directions   (directions = left, right by default)
                     does every state have a transition function for all symbols defined in sigma (deterministic fa)
 
-    Todo: test cases - 4 valid, 1 invalid
-    checklist:
-        valid     even number 
-                  divisible by 4 
+                    change return 0 in whichTransition() function to something better
+
+                    cases where the machine is stuck in a loop
 """ 
 
 """
+
 @definition : Reads the text file and returns the machine definition. Check README.md file for the
               text format of the file input
 @params : filename  -  file name of the text file input
@@ -24,6 +28,7 @@ import sys
             reject  -  the reject state from Q
             transition  - (current state, input, next state, direction)
                         -  set of all transition functions
+
 """
 
 def readMachine(filename):
@@ -54,14 +59,17 @@ def readMachine(filename):
 
 """
 
-def allStep(list_transition, start, accept, reject, word):
+def allStep(list_transition, start, accept, reject, word, sigma):
     flag=  True
     head=0 # this is the pointer to which character will be read from the word 
     curr_state = start
     accepted = False
     while(flag):
         _ = input("Step: ")
-        curr_state, direction = nextStep(list_transition, curr_state, word[head])
+        if validateSymbol(word[head], sigma):
+            curr_state, direction = nextStep(list_transition, curr_state, word[head])
+        else:
+            return False
         if direction =="left":
             head -=1
         else:
@@ -73,14 +81,30 @@ def allStep(list_transition, start, accept, reject, word):
         displayAction(curr_state, word[head], word, head)
     return accepted
 
-def quickStep(list_transition, start, accept, reject, words):
-    flag=  True
-    head=0 # this is the pointer to which character will be read from the word 
-    curr_state = start
-    accepted = False
+"""
+
+@definition: This gives accepts a list of string inputs and determines whether each string
+                is accepted or rejected by the machine
+@params:  list_transition  -  set of all transition functions defined in the machine definition
+            start  -  start state
+            accept = accept state
+            reject = reject state
+            words = list of words to be read
+@returns: accepted  - a list of booleans where true is for accepted and false otherwise
+
+"""
+
+def quickStep(list_transition, start, accept, reject, words, sigma):
+    accepted = []
     for word in words:
+        flag=  True
+        head=0 # this is the pointer to which character will be read from the word 
+        curr_state = start
         while(flag):
-            curr_state, direction = nextStep(list_transition, curr_state, word[head])
+            if validateSymbol(word[head], sigma):
+                curr_state, direction = nextStep(list_transition, curr_state, word[head])
+            else:
+                return False
             if direction =="left":
                 head -=1
             else:
@@ -88,15 +112,12 @@ def quickStep(list_transition, start, accept, reject, words):
             if isEnd(curr_state, accept, reject):
                 flag = False
                 if isAccepted(curr_state, accept):
-                    accepted = True
+                    accepted.append(True)
             displayAction(curr_state, word[head], word, head)
-        return accepted
+            accepted.append(False)
+    return accepted
 
 """
-
- I plan to do something on the transiion, check paper to recall
-transition is a list of transitions whose current state = state
-the appropriate transition list according to the state should be found before calling this function
 
 @definition: This gives the appropriate transition function according to the input given
 @params:  list_transition  -  set of all transition functions defined in the machine definition
@@ -108,12 +129,14 @@ the appropriate transition list according to the state should be found before ca
 
 def nextStep(list_transition, state, input):
     filtered_list = filterTransistionList(list_transition, state)
+
     for i in filtered_list:
         _, req_input, next_state, direction = i
         if req_input == input:
             return next_state, direction
         
 """
+
 @definition: This function filters the set all all transitions from the machine definition based
             on the current state
 @params:  list_transition  -  this is the set of all transitions defined in the machine definition
@@ -145,39 +168,160 @@ def whichTransition_Exact(list_transition, input):
             return next_state, direction
     return 0
 
-""""""
+"""
+
+@definition: This checks if the character input is in the alphabet of the machine
+@params:  character  -  the current character being read
+            sigma  -  the alphabet of all symbols to be read
+@returns: True  - character is in the alphabet
 
 """
+
+def validateSymbol(character, sigma):
+    if character in sigma:
+        return True
+    else:
+        print("Invalid input : input not in alphabet")
+        return False
+
+"""
+
+definition: This checks the all transition functions in the 
+            list provided by the machine definition is valid
+            A transition function is considered valid if: 
+                1. the input is in the alphabet union (left and right end markers)
+                2. the current state is in the set of states
+                3. the next state is in the set of states
+                4. the accept state has no outgoing transition
+                5. the reject state has no outgoing transition
+                6. the left end marker has no outgoing transition to the left
+                7. the right end marker has no outgoing transition to the right
+@params:  list_transition  -  set of all transition functions defined in the machine definition
+            sigma  -  the alphabet of all symbols to be read
+            Q  -  the set of all states
+            accept = accept state
+            reject = reject state
+@returns: True  - all transition functions are valid
+            False - atleast one transition function is invalid
+
+"""
+def validateTransition(sigma, list_transition, Q, accept, reject):
+    extended_sigma =('-','+')
+    extended_sigma.append(sigma)
+    for i in list_transition:
+        state, req_input, state2, direction = i
+        if req_input not in extended_sigma:
+            print("Invalid transition function : input not in alphabet")
+            print("transition function in question: "+ i)
+            return False
+        if state not in Q:
+            print("Invalid transition function : current state not in set of states")
+            print("transition function in question: "+ i)
+            return False
+        if state2 not in Q:
+            print("Invalid transition function : next state not in set of states")
+            print("transition function in question: "+ i)
+            return False
+        if state == accept and state2 != accept:
+            print("Invalid transition function : accept state cannot have an outgoing transition")
+            print("transition function in question: "+ i)
+            return False
+        if state == reject and state2 != reject:
+            print("Invalid transition function : reject state cannot have an outgoing transition")
+            print("transition function in question: "+ i)
+            return False
+        if req_input == '-'and direction == 'left':
+            print("Invalid transition function : cannot move left from left end marker")
+            print("transition function in question: "+ i)
+            return False
+        if req_input == '+'and direction == 'right':
+            print("Invalid transition function : cannot move right from right end marker")
+            print("transition function in question: "+ i)
+            return False
+    return True
+
+"""
+
+@definition: This checks if the machine is deterministic or not. A machine is deterministic if:
+             every state has a transition function for every input in the alphabet union (left and right end markers)
+
+@params:  list_transition  -  set of all transition functions defined in the machine definition
+            sigma  -  the alphabet of all symbols to be read
+            Q  -  the set of all states
+@returns: False - machine is non-deterministic, True otherwise
+
+"""
+
+def validateDeterministic(Q, sigma, list_transition):
+    extended_sigma = ('-','+')
+    extended_sigma.append(sigma)
+    for state in Q:
+        transitions = filterTransistionList(list_transition, state)
+        for i in extended_sigma:
+            if i not in transitions:
+                print("Machine is not deterministic")
+                print("state "+ state+" has no transition for input "+ i)
+                return False
+    return True
+
+"""
+note: might update this function. As of my current understanding, 
+the machine will end if it reaches either accepting state and reject state
+and if the head points to an end marker.
+
 @definition: This checks if the process should end terminate or not
 @params:  state   -  the current state
         accept  -  the acceptance state
         reject - the rejected state
 @returns: True  - process should terminate
           False - keep reading inputs
+
 """
 
-def isEnd(state, accept,reject):
-    if state == accept or state == reject:
+def isEnd(state, accept,reject, input):
+    if (state == accept or state == reject) and input in ('-','+'):
         return True
     else:
         return False
     
 """
+
 @definition: This checks if the process should end terminate or not
 @params:  state   -  the current state
         accept  -  the acceptance state
         reject - the rejected state
 @returns: True  - process should terminate
+
 """
+
 def isAccepted(state, accept):
     if state == accept:
         return True
     else:
         return False
 
+"""
+
+@definition: This encapsulates the word input with left and right end markers
+@params: word - the word to be encapsulated
+@return: -w+  - where w is the word input
+
+"""
+
 def attachEndMarker(word):
     return  '-' + word + '+'
-    
+
+"""
+
+HELPER FUNCTION 
+@definition: This displays the current state of the machine
+@params:  state   -  the current state
+        input  -  the current input
+        string - the word input
+        head - the pointer to the current character being read
+
+"""   
+
 def displayAction(state, input, string, head):
     print("Current State: "+state)
     print("Current Input: "+input)
@@ -185,10 +329,16 @@ def displayAction(state, input, string, head):
     print("head: "+head)
 
 def main():
-    _, _, start, accept, reject, list_transition = readMachine("test.txt")
+    Q, sigma, start, accept, reject, list_transition = readMachine("/tests/test.txt")
     #Q, sigma will be included once the verification is accomplished
     word = input("Enter Word: ")
     word = attachEndMarker(word)
+    if validateTransition(sigma, list_transition, Q, accept, reject) and validateDeterministic(Q, sigma, list_transition):
+        print("Machine is valid")
+    else:
+        print("Machine is invalid")
+        return 0
+
     """
     print("start: "  + start)
     print("accept: " + accept)
@@ -196,7 +346,7 @@ def main():
     for i in list_transition:
         print(i)
      """
-    accepted = allStep(list_transition, start, accept, reject, word)
+    accepted = allStep(list_transition, start, accept, reject, word, sigma)
     if(accepted):
         print("Accepted")
     else:
