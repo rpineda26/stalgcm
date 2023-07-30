66e489c4d7997876c89d083edfa23d93d79fa181
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-07-30 18:39:55 +0800

    feat: getters and setters for machine model

--- model.py
+++ model.py
@@ -1,12 +1,14 @@
 class Machine_2DFA:
     def __init__(self, Q, sigma, delta, start, accept, reject):
-        self.Q = Q
-        self.sigma = sigma
-        self.delta = delta
-        self.start = start
-        self.accept = accept
-        self.reject = reject
-        self.machine_definition_file = None
+        self.Q = None
+        self.sigma = None
+        self.delta = None
+        self.start = None
+        self.accept = None
+        self.reject = None
+    """
+    getters for the DFA
+    """
     def getQ(self):
         return self.Q
     def getSigma(self):
@@ -17,5 +19,28 @@ class Machine_2DFA:
         return self.start
     def getAccept(self):
         return self.accept
-    def setMachineDefinitionFile(self, machine_definition_file):
-        self.machine_definition_file = machine_definition_file
+    def getReject(self):
+        return self.reject
+  
+    """
+    setters for the DFA
+    """
+    def setQ(self, Q):
+        self.Q = Q
+    def setSigma(self, sigma):
+        self.sigma = sigma
+    def setDelta(self, delta):
+        self.delta = delta
+    def setStart(self, start):
+        self.start = start
+    def setAccept(self, accept):
+        self.accept = accept
+    def setReject(self, reject):
+        self.reject = reject
+    def resetMachine(self):
+        self.Q = None
+        self.sigma = None
+        self.delta = None
+        self.start = None
+        self.accept = None
+        self.reject = None
